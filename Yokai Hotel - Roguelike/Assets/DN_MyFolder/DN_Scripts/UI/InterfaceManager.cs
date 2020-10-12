f8934e713ed424972707dccb6fbc1d26dd09104c
Djason NATHIEZ <d.nathiez@rubika-edu.Com>
2020-10-12 18:11:16 +0200

    Milestones du 12 Octobre

---
+++ Yokai Hotel - Roguelike/Assets/DN_MyFolder/DN_Scripts/UI/InterfaceManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class InterfaceManager : MonoBehaviour
+{
+    InventoryManager inventory;
+
+    [Header("Health")]
+    public RawImage[] allTextureLantern;
+    public Texture hpActivate;
+    public Texture hpDown;
+    /// Texture hpNotActive;
+
+    int currentHealth;
+    int maxHealth;
+
+    [Header("Money")]
+    TextMeshProUGUI moneyText;
+    int currentMoney;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryManager>(); //initialization du script InventoryManager
+        moneyText = GameObject.FindGameObjectWithTag("MoneyText").GetComponent<TextMeshProUGUI>();
+
+        allTextureLantern = GetComponentsInChildren<RawImage>();
+
+        //initialization HealthBar
+        for(int i = 0; i < allTextureLantern.Length; i++)
+        {
+            if(i != 0)
+            {
+                allTextureLantern[i].enabled = true;
+            }
+        }
+
+    }
+
+    private void Update()
+    {
+
+        UpdateHealth();
+        UpdateMoney();
+    }
+
+    void UpdateHealth()
+    {
+        if (inventory)
+        {
+            currentHealth = inventory.currentHealth;
+            maxHealth = inventory.maxHealth;
+        }
+
+        for(int i = 0; i < allTextureLantern.Length; i++)
+        {
+            if(i != 0)
+            {
+                allTextureLantern[i].texture = (currentHealth >= i) ? hpActivate : hpDown; 
+            }
+        }
+    }
+
+    void UpdateMoney()
+    {
+        currentMoney = inventory.money;
+        moneyText.text = currentMoney.ToString();
+    }
+
+}
---
+++ Yokai Hotel - Roguelike/Assets/DN_MyFolder/DN_Scripts/xyzScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class xyzScript : MonoBehaviour
+{
+    public TextMeshProUGUI xyzText;
+    public Transform room;
+    public Transform gameObjectFind;
+    public Transform camera;
+
+    private void Update()
+    {
+        xyzText.text = room.transform.position.ToString() + "\n" + gameObjectFind.transform.position.ToString() + "\n" + camera.transform.position.ToString();
+    }
+}
