e3e8e5ee3ae3ddd877ce140213894210e0ceff06
Ezskel <k.faynot@rubika-edu.com>
2020-10-12 17:10:34 +0200

    Asset Layer Order fix

---
+++ Yokai Hotel - Roguelike/Assets/KF_MyFolder/KF_Scripts/KF_AssetLayerOrdering.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class KF_AssetLayerOrdering : MonoBehaviour
+{
+    private GameObject thisObject;
+    private SpriteRenderer thisSP;
+
+    private void Start()
+    {
+        thisObject = this.gameObject;
+        thisSP = thisObject.GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            thisSP.sortingOrder = -1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            thisSP.sortingOrder = 1;
+        }
+    }
+}
