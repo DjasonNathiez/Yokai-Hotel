1db83e1e4ac4331abdd4a24a24c1577787368152
Djason Nathiez <nathiez.djason@outlook.fr>
2020-11-06 17:04:45 +0100

    Ajout des sons et du menu des sons

---
+++ Yokai Hotel - Roguelike/Assets/DN_MyFolder/DN_Scripts/UI/SettingMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    public void SetVolumeSFX(float volume)
+    {
+        audioMixer.SetFloat("sfx", volume);
+    }
+
+    public void SetVolumeMUSIC(float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+    }
+}
