03cad470f6a44792d11061c7052da6a5700cd65a
Keanu <keanunohea@gmail.com>
2020-11-22 22:51:27 +0100

    Props + Different Room Versions

---
+++ Yokai Hotel - Roguelike/Assets/KF_MyFolder/KF_Scripts/KF_AssetSwap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KF_AssetSwap : MonoBehaviour
+{
+    public List<GameObject> assetSets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i =0; i < assetSets.Count; i++)
+        {
+            assetSets[i].SetActive(false);
+        }
+
+        GameObject chosenSet;
+        chosenSet = assetSets[Random.Range(0, assetSets.Count)];
+        chosenSet.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
