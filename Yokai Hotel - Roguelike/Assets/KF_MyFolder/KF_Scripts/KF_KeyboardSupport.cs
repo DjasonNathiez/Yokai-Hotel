bb283466c1c4d43a8bdaff004daf7701bf25d20f
Keanu <keanunohea@gmail.com>
2021-01-19 01:53:10 +0100

    Keyboard Support

---
+++ Yokai Hotel - Roguelike/Assets/KF_MyFolder/KF_Scripts/KF_KeyboardSupport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KF_KeyboardSupport : MonoBehaviour
+{
+    private bool keyboard; 
+    public bool ui;
+    public Sprite keyboardButton; //Don't forget to assign this!
+    private Sprite controllerButton;
+
+
+    void Start()
+    {
+        if (ui == false)
+            controllerButton = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+        if (ui == true)
+            controllerButton = this.gameObject.GetComponent<Image>().sprite;
+
+        keyboard = FindObjectOfType<KF_Unlockables>().keyboard;
+        if (keyboard == true)
+        {
+            if (ui == false)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = keyboardButton;
+            }
+            else
+            {
+                this.gameObject.GetComponent<Image>().sprite = keyboardButton;
+            }
+        }
+        if (keyboard == false)
+        {
+            if (ui == false)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = keyboardButton;
+            }
+            else
+            {
+                this.gameObject.GetComponent<Image>().sprite = keyboardButton;
+            }
+        }
+    }
+
+}
---
+++ Yokai Hotel - Roguelike/Assets/KF_MyFolder/KF_Scripts/Room/KF_SecretProba.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KF_SecretProba : MonoBehaviour
+{
+    public GameObject secret;
+    public int probability;
+    private bool runOnce;
+    private int proba;
+
+    private void Start()
+    {
+        secret.SetActive(false);
+
+        proba = Random.Range(0, probability);    // 1 in a [probability] chance to spawn.
+        if (proba == 0)
+        {
+            secret.SetActive(true);
+        }
+
+        
+        
+    }
+}
